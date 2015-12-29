d965075e1a67f80c6b875c8205a84530bf2f02d0
Paul M. Jones <pmjones88@gmail.com>
2015-12-29 01:24:49 -0600

    bare bones tests

---
+++ tests/FsioTest.php
@@ -0,0 +1,83 @@
+<?php
+namespace Atlas\Cli;
+
+class FsioTest extends \PHPUnit_Framework_TestCase
+{
+    protected $fsio;
+    protected $base;
+
+    protected function setUp()
+    {
+        $this->fsio = new Fsio();
+        $this->base = __DIR__ . DIRECTORY_SEPARATOR . 'tmp';
+    }
+
+    public function testIsDir()
+    {
+        $this->assertTrue($this->fsio->isDir(__DIR__));
+    }
+
+    protected function getPath($path)
+    {
+        return $this->base . DIRECTORY_SEPARATOR
+            . str_replace('/', DIRECTORY_SEPARATOR, $path);
+    }
+
+    public function testMkdir()
+    {
+        $dir = $this->getPath('fakedir');
+        if ($this->fsio->isDir($dir)) {
+            rmdir($dir);
+        }
+
+        $this->assertFalse($this->fsio->isDir($dir));
+        $this->fsio->mkdir($dir);
+        $this->assertTrue($this->fsio->isDir($dir));
+        rmdir($dir);
+        $this->assertFalse($this->fsio->isDir($dir));
+
+        $this->setExpectedException(
+            'Atlas\Cli\Exception',
+            'mkdir(): File exists'
+        );
+        $this->fsio->mkdir(__DIR__);
+    }
+
+    public function testGet()
+    {
+        $text = $this->fsio->get(__FILE__);
+        $this->assertSame('<?php', substr($text, 0, 5));
+
+        $this->setExpectedException(
+            'Atlas\Cli\Exception',
+            'No such file or directory'
+        );
+        $this->fsio->get($this->getPath('no-such-file'));
+    }
+
+    public function testPut()
+    {
+        $file = $this->getPath('fakefile');
+        if (file_exists($file)) {
+            unlink($file);
+        }
+
+        $expect = 'fake text';
+        $this->fsio->put($file, $expect);
+        $actual = $this->fsio->get($file);
+        $this->assertSame($expect, $actual);
+        unlink($file);
+
+        $file = $this->getPath('no-such-directory/fakefile');
+        $this->setExpectedException(
+            'Atlas\Cli\Exception',
+            'No such file or directory'
+        );
+        $this->fsio->put($file, $expect);
+    }
+
+    public function testGetCwd()
+    {
+        $this->assertSame(getcwd(), $this->fsio->getCwd());
+    }
+}
--- tests/Skeleton/SkeletonTest.php
+++ tests/Skeleton/SkeletonTest.php
@@ -7,10 +7,12 @@ use Aura\Cli\CliFactory;
 class SkeletonTest extends \PHPUnit_Framework_TestCase
 {
     protected $skeleton;
+    protected $stdout;
+    protected $stderr;
 
     protected function setUp()
     {
-        $this->fsio = new FakeFsio();
+        $this->fsio = $this->newFsio();
 
         $cliFactory = new CliFactory($GLOBALS);
         $this->stdio = $cliFactory->newStdio(
@@ -18,12 +20,66 @@ class SkeletonTest extends \PHPUnit_Framework_TestCase
             'php://memory',
             'php://memory'
         );
+        $this->stdout = $this->stdio->getStdout();
+        $this->stderr = $this->stdio->getStderr();
 
         $this->skeleton = new Skeleton($this->fsio, $this->stdio);
     }
 
+    protected function newFsio()
+    {
+        $fsio = new FakeFsio();
+
+        // put the real templates into the fake fsio
+        $dir = dirname(dirname(__DIR__)) . '/src/Skeleton/Templates';
+        $tpls = [
+            'Mapper.tpl',
+            'Plugin.tpl',
+            'Record.tpl',
+            'RecordSet.tpl',
+            'Table.tpl',
+        ];
+        foreach ($tpls as $tpl) {
+            $file = $dir . DIRECTORY_SEPARATOR . $tpl;
+            $fsio->put($file, file_get_contents($file));
+        }
+
+        return $fsio;
+    }
+
     public function test()
     {
-        $this->assertInstanceOf(Skeleton::CLASS, $this->skeleton);
+        $this->assertFalse($this->fsio->isFile('/app/DataSource/Author/AuthorMapper.php'));
+        $this->assertFalse($this->fsio->isFile('/app/DataSource/Author/AuthorPlugin.php'));
+        $this->assertFalse($this->fsio->isFile('/app/DataSource/Author/AuthorRecord.php'));
+        $this->assertFalse($this->fsio->isFile('/app/DataSource/Author/AuthorRecordSet.php'));
+        $this->assertFalse($this->fsio->isFile('/app/DataSource/Author/AuthorTable.php'));
+
+        $this->fsio->mkdir('/app/DataSource');
+
+        $input = new SkeletonInput();
+        $input->dir = '/app/DataSource';
+        $input->namespace = 'App\\DataSource\\Author';
+        $input->full = true;
+        $input->conn = ['sqlite:' . dirname(__DIR__) . DIRECTORY_SEPARATOR . 'fixture.sqlite'];
+        $input->table = 'authors';
+
+        $this->skeleton->__invoke($input);
+
+        $this->assertTrue($this->fsio->isFile('/app/DataSource/Author/AuthorMapper.php'));
+        $this->assertTrue($this->fsio->isFile('/app/DataSource/Author/AuthorPlugin.php'));
+        $this->assertTrue($this->fsio->isFile('/app/DataSource/Author/AuthorRecord.php'));
+        $this->assertTrue($this->fsio->isFile('/app/DataSource/Author/AuthorRecordSet.php'));
+        $this->assertTrue($this->fsio->isFile('/app/DataSource/Author/AuthorTable.php'));
+    }
+
+    protected function readHandle($handle)
+    {
+        $text = '';
+        $handle->rewind();
+        while ($read .= $handle->fread(8192)) {
+            $text .= $read;
+        }
+        return $text;
     }
 }
