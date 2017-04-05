37d384b8362071458c39773b3adc05f286486ca9
Paul M. Jones <pmjones88@gmail.com>
2017-04-05 09:56:12 -0500

    extend from AbstractLogger instead of writing all methods

--- src/Logger.php
+++ src/Logger.php
@@ -1,7 +1,14 @@
 <?php
+/**
+ *
+ * This file is part of Atlas for PHP.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ *
+ */
 namespace Atlas\Cli;
 
-use Psr\Log\LoggerInterface;
+use Psr\Log\AbstractLogger;
 use Psr\Log\LogLevel;
 use Aura\Cli\Stdio\Handle;
 
@@ -10,7 +17,7 @@ use Aura\Cli\Stdio\Handle;
  * A basic logger implementation that writes to a resource handle.
  *
  */
-class Logger implements LoggerInterface
+class Logger extends AbstractLogger
 {
     /**
      *
@@ -31,135 +38,6 @@ class Logger implements LoggerInterface
     public function __construct(Handle $handle)
     {
         $this->handle = $handle;
-    }
-
-    /**
-     *
-     * System is unusable.
-     *
-     * @param string $message
-     *
-     * @param array $context
-     *
-     * @return null
-     *
-     */
-    public function emergency($message, array $context = [])
-    {
-        $this->log(LogLevel::EMERGENCY, $message, $context);
-    }
-
-    /**
-     *
-     * Action must be taken immediately.
-     *
-     * @param string $message
-     *
-     * @param array $context
-     *
-     * @return null
-     *
-     */
-    public function alert($message, array $context = [])
-    {
-        $this->log(LogLevel::ALERT, $message, $context);
-    }
-
-    /**
-     *
-     * Critical conditions.
-     *
-     * @param string $message
-     *
-     * @param array $context
-     *
-     * @return null
-     *
-     */
-    public function critical($message, array $context = [])
-    {
-        $this->log(LogLevel::CRITICAL, $message, $context);
-    }
-
-    /**
-     *
-     * Runtime errors that do not require immediate action but should typically
-     * be logged and monitored.
-     *
-     * @param string $message
-     *
-     * @param array $context
-     *
-     * @return null
-     *
-     */
-    public function error($message, array $context = [])
-    {
-        $this->log(LogLevel::ERROR, $message, $context);
-    }
-
-    /**
-     *
-     * Exceptional occurrences that are not errors.
-     *
-     * @param string $message
-     *
-     * @param array $context
-     *
-     * @return null
-     *
-     */
-    public function warning($message, array $context = [])
-    {
-        $this->log(LogLevel::WARNING, $message, $context);
-    }
-
-    /**
-     *
-     * Normal but significant events.
-     *
-     * @param string $message
-     *
-     * @param array $context
-     *
-     * @return null
-     *
-     */
-    public function notice($message, array $context = [])
-    {
-        $this->log(LogLevel::NOTICE, $message, $context);
-    }
-
-    /**
-     *
-     * Interesting events.
-     *
-     * @param string $message
-     *
-     * @param array $context
-     *
-     * @return null
-     *
-     */
-    public function info($message, array $context = [])
-    {
-        $this->log(LogLevel::INFO, $message, $context);
-    }
-
-    /**
-     *
-     * Detailed debug information.
-     *
-     * @param string $message
-     *
-     * @param array $context
-     *
-     * @return null
-     *
-     */
-    public function debug($message, array $context = [])
-    {
-        $this->log(LogLevel::DEBUG, $message, $context);
     }
 
     /**
